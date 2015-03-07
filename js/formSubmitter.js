517475e1f9a32870a6d3a24c360063cbac3dc971
Robert Clabough <rmcn96@gmail.com>
2015-03-07 15:50:57 -0600

    Functional

--- js/formSubmitter.js
+++ js/formSubmitter.js
@@ -23,19 +23,19 @@ $('document').ready(function() {
 	function packageVals() {
 		var json_object = {
 			PrecinctRevGoal : {
-				rev_current : $('#precinctRev_current').val(),
-				rev_month : $('#precinctRev_month').val(),
-				rev_goal : $('#precinctRev_goal').val()
+				current : $('#precinctRev_current').val(),
+				month : $('#precinctRev_month').val(),
+				goal : $('#precinctRev_goal').val()
 			},
 			MonthlyBudget : {
-				budget_current : $('#budget_current').val(),
-				budget_month : $('#budget_month').val(),
-				budget_goal : $('#budget_goal').val()
+				current : $('#budget_current').val(),
+				month : $('#budget_month').val(),
+				goal : $('#budget_goal').val()
 			},
 			NPS : {
-				nps_current : $('#nps_current').val(),
-				nps_month : $('#nps_month').val(),
-				nps_goal : $('#budget_goal').val()
+				current : $('#nps_current').val(),
+				month : $('#nps_month').val(),
+				goal : $('#budget_goal').val()
 			}
 		}
 		json_string = JSON.stringify(json_object);
